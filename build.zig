c653dc7619f75a1af61e8c5be9dd7e76b50b59cc
Robert Williamson <robert11122002@gmail.com>
2023-11-30 22:37:05 +0000

    initial

---
+++ build.zig
@@ -0,0 +1,36 @@
+const std = @import("std");
+
+pub fn build(b: *std.Build) void {
+    const target = b.standardTargetOptions(.{});
+    const optimize = b.standardOptimizeOption(.{});
+
+    comptime var dayNumber: u8 = 1;
+    inline while (dayNumber <= 1) : (dayNumber += 1) {
+        setupDay(b, target, optimize, dayNumber);
+    }
+}
+
+fn setupDay(b: *std.Build, target: std.zig.CrossTarget, optimize: std.builtin.OptimizeMode, dayNumber: u8) void {
+    const path = b.fmt("day{d:0>2}", .{dayNumber});
+    const root = b.fmt("{s}/main.zig", .{path});
+    const exe = b.addExecutable(.{
+        .name = path,
+        .root_source_file = .{ .path = root },
+        .target = target,
+        .optimize = optimize,
+    });
+
+    b.installArtifact(exe);
+    const install_step = b.step(path, "Build the specified day");
+    install_step.dependOn(b.getInstallStep());
+
+    const run_cmd = b.addRunArtifact(exe);
+    run_cmd.step.dependOn(install_step);
+
+    if (b.args) |args| {
+        run_cmd.addArgs(args);
+    }
+
+    const run_step = b.step(b.fmt("run_{s}", .{path}), "Run the specified day");
+    run_step.dependOn(&run_cmd.step);
+}

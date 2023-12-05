9b1ad62de5595aea0aa3b79ac13d98269bd09e51
Robert Williamson <robert11122002@gmail.com>
2023-12-05 15:24:42 +0000

    still not done

--- day05/main.zig
+++ day05/main.zig
@@ -4,33 +4,62 @@ const input = @embedFile("./sample");
 // seed-to-soil is soil then seed value
 
 const Key = struct {
-    S,
-    u64,
+    keyType: S,
+    keyStart: u64,
+    range: u64,
 };
 var map: std.AutoHashMap(Key, u64) = undefined;
 var seeds: std.ArrayList(u64) = undefined;
-var lastIdx: usize = undefined;
 
-pub fn main() void {
+pub fn main() !void {
     var arena = std.heap.ArenaAllocator.init(std.heap.page_allocator);
     defer arena.deinit();
     const allocator = arena.allocator();
 
-    map.init(allocator);
+    map = std.AutoHashMap(Key, u64).init(allocator);
     defer map.deinit();
 
-    seeds = std.ArrayList(u8).init(allocator);
+    seeds = std.ArrayList(u64).init(allocator);
     defer seeds.deinit();
+
+    try parseMaps(input);
 }
 
 fn parseMaps(maps: []const u8) !void {
     const loc = std.mem.indexOf(u8, maps, ":").?;
     const line = std.mem.indexOf(u8, maps, "\n").?;
-    var numberIter = std.mem.tokenizeScalar(u8, maps[loc..line], ' ');
+    var numberIter = std.mem.tokenizeScalar(u8, maps[loc + 1 .. line], ' ');
 
     while (numberIter.next()) |numberStr| {
         const number = try std.fmt.parseInt(u8, numberStr, 10);
         try seeds.append(number);
+    }
+
+    // seed to soil.
+    const seedToSoil = maps[line..];
+    var iterator = std.mem.tokenizeSequence(u8, seedToSoil, "\n");
+    try captureGroup(&iterator, .seed);
+    try captureGroup(&iterator, .soil);
+    try captureGroup(&iterator, .fertilizer);
+    try captureGroup(&iterator, .water);
+    try captureGroup(&iterator, .light);
+    try captureGroup(&iterator, .temperature);
+    try captureGroup(&iterator, .humidity);
+}
+
+fn captureGroup(iterator: *std.mem.TokenIterator(u8, .sequence), keyType: S) !void {
+    _ = iterator.next();
+    while (iterator.next()) |lines| {
+        if (!switch (lines[0]) {
+            '0'...'9' => true,
+            else => false,
+        }) break;
+        var keyValueIter = std.mem.tokenizeScalar(u8, lines, ' ');
+        const value = try std.fmt.parseInt(u64, (keyValueIter.next().?), 10);
+        const keyStart = try std.fmt.parseInt(u64, (keyValueIter.next().?), 10);
+        const range = try std.fmt.parseInt(u64, (keyValueIter.next().?), 10);
+
+        try map.put(.{ .keyType = keyType, .keyStart = keyStart, .range = range }, value);
     }
 }
 
@@ -45,8 +74,10 @@ const S = enum {
     soil,
     fertilizer,
     water,
-    ligh,
+    light,
     temperature,
     humidity,
+
+    /// may not necessarily use this one
     location,
 };
